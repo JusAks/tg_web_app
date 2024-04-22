fef70130bd0f503dafc86dc4df99bec54f3e7ee6
Just-Aks <serhii.aksonov.work@gmail.com>
2024-04-22 16:49:01 +0300

    expand Test

--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -1,16 +1,26 @@
-import React , { useState } from 'react';
+import React , { useEffect , useState } from 'react';
 import st from './Main.module.scss'
 
 let tg =window.Telegram.WebApp;
 const Main = ()=>{
+    useEffect ( ()=>{
+        tg.ready();
+        tg.expand();
+    } , [] );
     console.log(tg.answerWebAppQuery)
     console.log(tg.viewportStableHeight)
+    console.log(tg.isExpanded)
     const [text,setText]=useState(null)
-    debugger
+    const doExpand=()=>{
+        tg.expand()
+    }
+   
     return (
         <div className={st.container}>
             {text&&text}
             <button onClick={()=>{setText(tg.viewportHeight)}}>send Data</button>
+            {tg.isExpanded.toString()}
+            <button onClick={doExpand}>a</button>
            
             
         </div>
