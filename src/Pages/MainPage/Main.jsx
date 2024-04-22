9aeddd342dd438b6ee37a527112b8efc55443bbd
Just-Aks <serhii.aksonov.work@gmail.com>
2024-04-22 17:22:06 +0300

    know you

--- src/Pages/MainPage/Main.jsx
+++ src/Pages/MainPage/Main.jsx
@@ -3,26 +3,29 @@ import st from './Main.module.scss'
 
 let tg =window.Telegram.WebApp;
 const Main = ()=>{
+    const [data,setData] = useState(null)
     useEffect ( ()=>{
         tg.ready();
         tg.expand();
+        
     } , [] );
-    console.log(tg.answerWebAppQuery)
-    console.log(tg.viewportStableHeight)
-    console.log(tg.isExpanded)
     const [text,setText]=useState(null)
-    const doExpand=()=>{
-        tg.expand()
+    const knowYou=()=>{
+        setData(tg.initDataUnsafe.user)
     }
    
     return (
         <div className={st.container}>
             {text&&text}
             <button onClick={()=>{setText(tg.viewportHeight)}}>send Data</button>
-            {tg.isExpanded.toString()}
-            <button onClick={doExpand}>a</button>
-           
-            
+            <div>
+                {data && 
+                    data?.first_name+" "+data?.last_name
+                }
+                <button onClick={ knowYou }>knowYou</button>
+            </div>
+
+
         </div>
     );
 };
