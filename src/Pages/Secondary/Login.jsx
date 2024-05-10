be93f134812c09b00b70de4c9139109e71bea30c
Just-Aks <serhii.aksonov.work@gmail.com>
2024-05-10 14:55:22 +0300

    test trans

--- src/Pages/Secondary/Login.jsx
+++ src/Pages/Secondary/Login.jsx
@@ -1,21 +1,82 @@
 import React , { useEffect , useState } from 'react';
+import { useTonAddress , useTonConnectUI } from "@tonconnect/ui-react";
 
+/*const secondaryAddress="0QAGrOtKF4KUWwWJD49KgjF8qSlo7083762sSfMTwkLZTqoN"
+const secondaryAddress1="0:977e12d945a67a3a48b63ca98f18efb8064f4277f6ecec53417272e6e642396c"*/
+const tonkeeperMainWalletMainnet = 'UQCXfhLZRaZ6Oki2PKmPGO-4Bk9Cd_bs7FNBcnLm5kI5bHPK'
 
 const Login = ()=>{
-    
+    const [tonConnectUI, setOptions] = useTonConnectUI();
+    const address = useTonAddress()
+   /* const addressTr = useTonAddress(false)*/
+    const UIAddress = address.slice(0,4)+"..."+address.slice(-4)
+    /*const wallet = useTonWallet();
+    const [info,setInfo]=useState()*/
     const [isAuth,setIsAuth]=useState(false)
-
+    
     useEffect ( ()=>{
-        setIsAuth(true)
-    } , [] );
+        setOptions({ language: "ru" });
+        if(tonConnectUI.account){
+            setIsAuth(true)
+        }
+        if(tonConnectUI.account === null){
+            setIsAuth(false)
+        }
+    } , [tonConnectUI.account] );
+    
+    const testFunk2= async ()=>{
+        console.log(address)
+       /*console.log(addressTr)
+        tonConnectUI.openSingleWalletModal("Tonkeeper")
+        
+       let a = tonConnectUI.modal
+        console.log(a)
+       /!* 
+        await tonConnectUI.openModal().then(res=>{debugger})
+        await  tonConnectUI.getWallets().then(res=>{debugger})*!/
+        debugger*/
+    }
+    
+    const transaction = {
+        validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+        messages: [
+            {
+                address: tonkeeperMainWalletMainnet, // destination address
+                amount: "20000000" //Toncoin in nanotons
+            }
+        ]
+
+    }
+    const testTransaction= async ()=>{
+        setOptions({ language: "en" });
+        try {
+           await tonConnectUI.sendTransaction(transaction)
+        }
+        catch (e){
+            console.log(e)
+            debugger
+        }
+        
+    }
+    const connect= async ()=>{
+        try {
+            await tonConnectUI.openModal()
+        }
+        catch (e){
+            console.log(e)
+        }
+    }
     
     return (
         <div className='loginContainer'>
             <div className='loginStatus'>
-                {isAuth?'Welcome!': 'Not Authorized!'} 
+                {isAuth?'Welcome! '+UIAddress: 'Not Authorized!'}
             </div>
             <div className='loginAction'>
-                <button>Connect Wallet</button>
+                <button disabled={isAuth} onClick={ connect }>Connect Wallet</button>
+                <button onClick={  testFunk2}>test button</button>
+                <button disabled={!isAuth} onClick={  ()=>{tonConnectUI.disconnect()}}>disconnect</button>
+                <button disabled={!isAuth} onClick={testTransaction}>send 0.2 ton</button>
             </div>
         </div>
     );
